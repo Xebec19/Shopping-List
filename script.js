b27eda2489b478bbf3d4589efa64b29c559fe088
Rohan Kumar Thakur <rohant946@gmail.com>
2020-06-23 22:51:52 +0530

    Added HTML, CSS & JavaScript

---
+++ script.js
@@ -0,0 +1,28 @@
+var input = document.getElementById("userInput");
+var button = document.getElementById("enter");
+var ul = document.querySelector("ul");
+function Length()
+{
+	return input.value.length;
+}
+function AddValue()
+{
+	var li = document.createElement("li");	
+	li.appendChild(document.createTextNode(input.value));	
+	ul.appendChild(li);
+	input.value="";
+}
+function CallAddValue()
+{	if(Length() > 0)
+	{	
+		AddValue();	
+	}
+}
+function CallAddValueForKeyPress(event)
+{	if(Length() > 0 && eventKeyCode === 13)
+	{	
+		AddValue();	
+	}
+}
+button.addEventListener("click",CallAddValue);	
+input.addEventListener("enterpress",CallAddValueForKeyPress);
