eab542bf98beead38adaeb038a892e736570f2ee
xebec19 <thakur190420@gmail.com>
2020-10-18 14:45:23 +0530

    Added delete button

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
 var input = document.getElementById("userInput");
 var button = document.getElementById("enter");
 var ul = document.querySelector("ul");
+//var deleteBtns = document.getElementsByClassName("delete")
 function Length()
 {
 	return input.value.length;
 }
+//From StackOverflow
+function removeParent(evt){
+	evt.target.removeEventListener("click",removeParent,false);
+	evt.target.parentNode.remove();
+}
 function AddValue()
 {
+	var btn = document.createElement("button");
+	btn.innerHTML = "Delete";
+	btn.classList.add("btn-outline-primary");
+	btn.classList.add("btn-sm");
+	btn.onclick = removeParent;
+
 	var li = document.createElement("li");	
 	li.appendChild(document.createTextNode(input.value));
-	 li.classList.add("list-group-item");	
+	li.innerHTML += '&nbsp&nbsp&nbsp&nbsp';
+	li.classList.add("list-group-item");
+	li.appendChild(btn);	
 	ul.appendChild(li);
 	input.value="";
 }
